73c60dc1c646e871414eab97c069828dd3c6d465
Keith Tomlinson <ktomlinson@brighton-science.com>
2024-09-02 10:39:55 +0200

    scaffols basic ui with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/95344484-82f0-4539-92ce-ffff84ba40c4-edfqla.jpg",
+  "https://utfs.io/f/d14947d0-2f70-40e6-b651-b6c8f5326363-edfql9.jpg",
+  "https://utfs.io/f/41a12d4e-0d20-41f4-adce-753be3b926e8-edfql8.jpg",
+  "https://utfs.io/f/aa025b88-76c8-47fa-bb7b-710de6716a7a-edfql7.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => {
+  return {
+    id: index + 1,
+    url,
+  };
+});
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       Hello (gallery in progress)
     </main>
   );
