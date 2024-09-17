a4aeee33ffad5d85a1ea0595e1869b4857f1550a
Keith Tomlinson <ktomlinson@brighton-science.com>
2024-09-17 09:05:05 +0200

    links db to ui, removes mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,26 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/95344484-82f0-4539-92ce-ffff84ba40c4-edfqla.jpg",
-  "https://utfs.io/f/d14947d0-2f70-40e6-b651-b6c8f5326363-edfql9.jpg",
-  "https://utfs.io/f/41a12d4e-0d20-41f4-adce-753be3b926e8-edfql8.jpg",
-  "https://utfs.io/f/aa025b88-76c8-47fa-bb7b-710de6716a7a-edfql7.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => {
-  return {
-    id: index + 1,
-    url,
-  };
-});
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <pre key={post.id}>{post.name}</pre>
-        ))}
+        {[...images, ...images, ...images, ...images, ...images].map(
+          (image, index) => (
+            <div
+              key={image.id + "-" + index}
+              className="flex w-48 flex-col justify-between gap-2"
+            >
+              <img src={image.url} />
+              <div>{image.name}</div>
+            </div>
+          ),
+        )}
       </div>
-      <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} />
-          </div>
-        ))}
-      </div>
-      Hello (gallery in progress)
     </main>
   );
 }
